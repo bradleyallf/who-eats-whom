c48aff05e35db645e4d81de7e597c07b8f1e7602
sallf <sallf@saylorallf.com>
2022-03-19 21:53:46 -0600

    footer adjustment

--- src/components/layouts/Main/Footer/index.jsx
+++ src/components/layouts/Main/Footer/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './index.scss';
 
 const Footer = () => {
   // --------------------- ===
   //  RENDER
   // ---------------------
   return (
-    <footer>
+    <footer className={styles.footer}>
       footer
     </footer>
   );
