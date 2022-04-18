09902e42fe5b835759097341393ea5d44ca66a60
Bradley Allf <ballf@ncsu.edu>
2022-04-18 00:06:27 -0400

    hello bradley next commit

--- src/screens/HowToSubmit/index.jsx
+++ src/screens/HowToSubmit/index.jsx
@@ -6,7 +6,14 @@ const HowToSubmit = () => {
   //  RENDER
   // ---------------------
   return (
-    <div />
+    <div>
+      <p>
+        this is a paragraph. I'm writing in html
+      </p>
+      normal text. good to line up the tags i.e. greater than less than symbols. Turned on line-wrap. 
+      tab and shift-tab 
+
+    </div>
   );
 };
 
