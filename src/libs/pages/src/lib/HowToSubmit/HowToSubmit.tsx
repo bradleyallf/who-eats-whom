94521624f1a03202c9c2e12bc5bb70c6dd20eddd
Bradley Allf <ballf@ncsu.edu>
2023-10-20 12:32:38 -0600

    Simple language edits

--- src/libs/pages/src/lib/HowToSubmit/HowToSubmit.tsx
+++ src/libs/pages/src/lib/HowToSubmit/HowToSubmit.tsx
@@ -4,15 +4,17 @@ export const HowToSubmit = () => {
   // ---------------------
   return (
     <div>
-      <header className="entry-header alignwide">
-        <h1 className="entry-title">How to Submit</h1>
-      </header>
       To add your own observations to Who Eats Whom, first sign up for
       iNaturalist, then
-      <a href="https://www.inaturalist.org/projects/who-eats-whom">
+      <a href="https://www.inaturalist.org/projects/who-eats-whom" style={{ color: 'blue' }}>
         {' '}
         join our project here.{' '}
       </a>
+      For an observation to be included in the database, you must submit two observations
+      to the project: one observation of the "eating" organism and another observation 
+      of the organism being eaten. If both these observations are identified to 
+      "research-grade" by the iNaturalist community, the observation will be added to 
+      the Who Eats Whom database on this website.
     </div>
   )
 }
