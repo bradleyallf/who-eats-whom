022128ad82e51ee72ff4b800adebf1cfd4373bcd
Bradley Allf <71990775+bradleyallf@users.noreply.github.com>
2026-02-01 19:47:39 -0700

    Update text prompt for species input

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,7 +9,7 @@ export const Home = () => {
       <div className="col-12 text-center">
         <h1 className="text-4xl font-bold">Who Eats Whom</h1>
         <h2 className="text-sm">
-          Enter the common name of a species to see who it eats, or who eats it.
+          Enter the name of a species to see who it eats, or who eats it.
           For example, try "Who is eaten by" "Osprey."
         </h2>
         <a
