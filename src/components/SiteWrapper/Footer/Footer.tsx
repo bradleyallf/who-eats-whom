bca2c6510ebed5b7969b8ea70d1e74bed9716da6
Maithili Bhoop <mbhoop@ncsu.edu>
2026-02-06 00:57:16 -0500

    Added Citations to About Page

--- src/components/SiteWrapper/Footer/Footer.tsx
+++ src/components/SiteWrapper/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   //  RENDER
   // ---------------------
   return (
-    <footer className="bg-slate-200 text-center px-4 py-8 text-xs">
+    <footer className="bg-slate-200 text-center px-4 py-8 text-xs mt-12">
       &#169; Bradley Allf 2023
     </footer>
   )
